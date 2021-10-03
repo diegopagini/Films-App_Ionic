7fc29d93f0277aa6646289b48b698c529de45152
Diego Pagini <diegopaginidev@gmail.com>
2021-10-03 09:41:25 +0200

    cast

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { CreditsResponse, MovieDetail } from 'src/app/interfaces/interfaces';
 import { MoviesService } from 'src/app/services/movies.service';
@@ -12,11 +13,25 @@ export class DetailsComponent implements OnInit {
   @Input() id: number;
   public movieDetails$: Observable<MovieDetail>;
   public movieCast$: Observable<CreditsResponse>;
+  public hidden = 150;
+  public slidesOpts: Object = {
+    slidesPerView: 3.3,
+    freeMode: true,
+  };
 
-  constructor(private moviesService: MoviesService) {}
+  constructor(
+    private moviesService: MoviesService,
+    private modalController: ModalController
+  ) {}
 
   ngOnInit() {
     this.movieDetails$ = this.moviesService.getMovieDetail(this.id);
     this.movieCast$ = this.moviesService.getMovieActors(this.id);
   }
+
+  public back() {
+    this.modalController.dismiss();
+  }
+
+  public favorite() {}
 }
